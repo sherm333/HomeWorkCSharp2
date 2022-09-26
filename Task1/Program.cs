2d35144bd3426d00dd67b635afcf848c315e7099
sherm3 <mamafiendaasd@gmail.com>
2022-09-26 16:10:12 +0300

    vscode

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+*/
+
+int GetRandomNumber(int leftRange, int rightRange)
+{
+    Random rnd = new Random();
+    int result = rnd.Next(leftRange, rightRange);
+    return result;
+}
+
+int PrintSecondDigit(int number)
+{
+    int secondDigit = (number / 10) % 10;
+    return secondDigit;
+}
+
+int number = GetRandomNumber(100,1000);
+Console.WriteLine(number);
+
+int secDigit = PrintSecondDigit(number);
+Console.WriteLine(secDigit);
+
